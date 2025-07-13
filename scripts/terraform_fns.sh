ae9bb3112f5262177e27a5fbbb995aa645a19395
xoffio <marquessricardo@gmail.com>
2025-07-13 19:12:31 -0400

    Check load balancer before executing ansible

--- scripts/terraform_fns.sh
+++ scripts/terraform_fns.sh
@@ -35,5 +35,16 @@ check_ssh_connection() {
 		echo "      SSH is up in $ip"
 	done
 
-	# TODO: Load balancers
+	# TODO: Only do this if the cluster will be HA
+	echo ""
+	echo "[*] Making sure the load balancer nodes are ready to be setup..."
+	for n in "${CONTROL_NODE_LB_IPS[@]}"; do
+		ip="${n%/*}"
+
+		echo "    Waiting for SSH on $ip..."
+		while ! nc -z -w2 "$ip" 22 2>/dev/null; do
+			sleep 1
+		done
+		echo "      SSH is up in $ip"
+	done
 }
